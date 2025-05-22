2d26c17a3bd92f58d5d14fcc81f20435d18fdb1c
jayanthyp <jayanthkumaryp1@gmail.com>
2025-05-22 20:07:38 +1000

    fixed non loading website issue

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const morgan = require('morgan');
 const fs = require('fs');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 // Middleware
 app.use(cors());
